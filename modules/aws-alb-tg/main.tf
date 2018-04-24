001fbab5cb33acf7ef7eaac94d0fc3f56dfc33fa
Kostiantyn Kalynovskyi <kkalynovskyi@mirantis.com>
2018-04-25 02:26:31 +0300

    changing module url

--- modules/aws-alb-tg/main.tf
+++ modules/aws-alb-tg/main.tf
@@ -1,5 +1,5 @@
 module "aws-alb-tg" {
-  source          = "github.com/unicanova/terraform-aws-alb-tg"
+  source          = "git::https://github.com/unicanova/terraform-aws-alb-tg.git?ref=master"
   certificate_arn = "arn:aws:acm:us-east-1:329942816198:certificate/338d2752-79c7-4cb6-a022-eb2f0d558704"
   region          = "us-east-1"
   alb_name        = "alb-dev"
